dd95e908515e406df43e0f04875071333856c35b
Z_Daoudi <114952743+zackariadaoudi@users.noreply.github.com>
2023-06-16 18:44:32 +0100

    Add files via upload

---
+++ pdf.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import pandas as pd
+import requests
+
+df = pd.read_csv('arxiv_data.csv')
+
+print("key",df)
+matrix_data = df.to_numpy()
+Len = len(matrix_data)
+for i in range(Len) :
+ filename = Path(matrix_data[i,0])
+ arxiv_url = f"https://arxiv.org/pdf/{matrix_data[i,3]}.pdf"
+ print("The filename :", filename)
+ print("the url :", arxiv_url)
+ response = requests.get(arxiv_url)
+ filename.write_bytes(response.content)
+
