c763bfa2147596aaff68cd7c030fd9caa74d0ae7
Z_Daoudi <zackaria.daoudi@gmail.com>
2023-06-17 18:35:56 +0100

    new

--- import_pdf.py
+++ import_pdf.py
@@ -13,9 +13,12 @@ matrix_data = df.to_numpy()
 Len = len(matrix_data)
 for i in range(Len) :
  filename = Path(matrix_data[i,0])
+ category = Path(f"{matrix_data[i,2]}/{matrix_data[i,0]}.pdf")
  arxiv_url = f"https://arxiv.org/pdf/{matrix_data[i,3]}.pdf"
+ print("--"*50)
  print("The filename :", filename)
+ print("The category .", matrix_data[i,2])
  print("the url :", arxiv_url)
  response = requests.get(arxiv_url)
- Path("/pdf/").write_bytes(response.content)
+ category.write_bytes(response.content)
 
--- scraper.sh
+++ scraper.sh
@@ -1,5 +1,5 @@
 chmod +x $PWD
 
-mkdir astro-ph cond-mat gr-qc hep-ex hep-lat hep-ph hep-th math-ph nlin nucl-ex nucl-th quant-ph
+mkdir -p Physics/astro-ph Physics/cond-mat Physics/gr-qc Physics/hep-ex Physics/hep-lat Physics/hep-ph Physics/hep-th Physics/math-ph Physics/nlin Physics/nucl-ex Physics/nucl-th Physics/quant-ph 'Computer Science'/cs.FL
 
 python3 import_pdf.py
