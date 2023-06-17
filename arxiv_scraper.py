61a95d0d643dcacef5ccb87b3ec0d6e909dd8493
Z_Daoudi <zackaria.daoudi@gmail.com>
2023-06-17 22:26:37 +0100

    new

--- arxiv_scraper.py
+++ arxiv_scraper.py
@@ -3,7 +3,7 @@ import os, sys, stat
 import pandas as pd
 from tqdm.auto import tqdm
 import requests
-import scraper_arxiv as scraper
+import arxiv_data as scraper
 
 subject = ["Physics", "Mathematics", "Computer_Science", "Biology", "Engineering", "Statistics", "Economics", "Finance"]
 
@@ -11,35 +11,44 @@ subfield_physics = ["astro-ph", "cond-mat", "gr-qc", "hep-ex", "hep-lat", "hep-p
 
 subfield_maths = ["math.AG", "math.AT", "math.AP", "math.CT", "math.CA", "math.CO", "math.AC", "math.CV", "math.DG", "math.DS", "math.FA", "math.GM", "math.GN", "math.GT", "math.GR", "math.HO", "math.IT", "math.KT", "math.LO", "math.MP", "math.MG", "math.NT", "math.MA", "math.OA", "math.OC", "math.PR", "math.QA", "math.RT", "math.RA", "math.SP", "math.ST", "math.SG"]
 
+print(" " * 40,"----------------------------------------")
+print(" " * 40,"|           Zakaria Daoudi              |")
+print(" " * 40,"|      Arxiv Lecture Notes Scraper      |")
+print(" " * 40,"----------------------------------------\n")
 i = 0
 while i < len(subject):
 	print(i,subject[i])
 	i += 1 
- 
+
+print("-"*30) 
 choice = input("Choose the subject you want to scrape.\n")
 choice_subject = str(choice)
+print("-"*30) 
 x = 0
+print("The scraper is running, please wait until it's done. You can find later the data in the file arxiv_data.csv ordered by title, author, category and arXiv reference number")
+print(" "*30) 
+print(" "*30) 
+print(" "*30) 
 while x == 0 :
 	URL_arxiv = f"https://arxiv.org/search/?searchtype=all&query=lecture+notes&abstracts=show&size=200&order=-announced_date_first&start={x}"
 	scraper.arxiv(URL_arxiv,choice_subject)
-	print(x)
 	x += 200     	
 
 df = pd.read_csv('arxiv_data.csv')
+print("=="*50)
 print("key",df)
 matrix_data = df.to_numpy()
 Len = len(matrix_data)
 for i in range(Len) :
-	filename = Path(matrix_data[i,0])
-	category = Path(f"{matrix_data[i,2]}/{matrix_data[i,0]}.pdf")
-	arxiv_url = f"https://arxiv.org/pdf/{matrix_data[i,3]}.pdf"
-	print("--"*50)
-	print("The filename :", filename)
-	print("The category .", matrix_data[i,2])
-	print("the url :", arxiv_url)
-	response = requests.get(arxiv_url)
-	total_size_in_bytes= int(response.headers.get('content-length', None))
-	progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-	progress_bar.update(len(response.content))
-	category.write_bytes(response.content)
+        filename = Path(matrix_data[i,0])
+        category = Path(f"{choice_subject}/{matrix_data[i,2]}/{matrix_data[i,0]}.pdf")
+        arxiv_url = f"https://arxiv.org/pdf/{matrix_data[i,3]}.pdf"
+        print("=="*50)
+        print(" "*30,"The download is starting"," "*30)
+        print("The filename :", filename)
+        print("The category .", matrix_data[i,2])
+        print("the url :", arxiv_url)
+        response = requests.get(arxiv_url)
+        category.write_bytes(response.content)
+        print(f"{filename} download successfully.")
 
--- scraper.sh
+++ scraper.sh
@@ -2,4 +2,4 @@ chmod +x $PWD
 
 mkdir -p Physics/astro-ph Physics/cond-mat Physics/gr-qc Physics/hep-ex Physics/hep-lat Physics/hep-ph Physics/hep-th Physics/math-ph Physics/nlin Physics/nucl-ex Physics/nucl-th Physics/quant-ph 'Computer Science'/cs.FL
 
-python3 import_pdf.py
+python3 arxiv_scraper.py
