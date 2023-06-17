b83027cd3d86f9a9b3f1c15c0532fc2ce53396b5
Z_Daoudi <zackaria.daoudi@gmail.com>
2023-06-17 21:02:37 +0100

    new

--- import_pdf.py
+++ import_pdf.py
@@ -4,21 +4,34 @@ import pandas as pd
 import requests
 import scraper_arxiv as scraper
 
+subject = ["Physics", "Mathematics", "Computer_Science", "Biology", "Engineering", "Statistics", "Economics", "Finance"]
 
-scraper.arxiv()
+subfield_physics = ["astro-ph", "cond-mat", "gr-qc", "hep-ex", "hep-lat", "hep-ph", "hep-th", "math-ph", "nlin", "nucl-ex", "nucl-th", "physics", "quant-ph"]
 
+subfield_maths = ["math.AG", "math.AT", "math.AP", "math.CT", "math.CA", "math.CO", "math.AC", "math.CV", "math.DG", "math.DS", "math.FA", "math.GM", "math.GN", "math.GT", "math.GR", "math.HO", "math.IT", "math.KT", "math.LO", "math.MP", "math.MG", "math.NT", "math.MA", "math.OA", "math.OC", "math.PR", "math.QA", "math.RT", "math.RA", "math.SP", "math.ST", "math.SG"]
+
+i = 0
+while i < len(subject):
+	print(i,subject[i])
+	i += 1 
+ 
+choice = input("Choose the subject you want to scrape.\n")
+choice_subject = str(choice)
+ 
+scraper.arxiv(choice_subject)      	
 df = pd.read_csv('arxiv_data.csv')
 print("key",df)
 matrix_data = df.to_numpy()
 Len = len(matrix_data)
+
 for i in range(Len) :
- filename = Path(matrix_data[i,0])
- category = Path(f"{matrix_data[i,2]}/{matrix_data[i,0]}.pdf")
- arxiv_url = f"https://arxiv.org/pdf/{matrix_data[i,3]}.pdf"
- print("--"*50)
- print("The filename :", filename)
- print("The category .", matrix_data[i,2])
- print("the url :", arxiv_url)
- response = requests.get(arxiv_url)
- category.write_bytes(response.content)
+	filename = Path(matrix_data[i,0])
+	category = Path(f"{matrix_data[i,2]}/{matrix_data[i,0]}.pdf")
+	arxiv_url = f"https://arxiv.org/pdf/{matrix_data[i,3]}.pdf"
+	print("--"*50)
+	print("The filename :", filename)
+	print("The category .", matrix_data[i,2])
+	print("the url :", arxiv_url)
+	response = requests.get(arxiv_url)
+	category.write_bytes(response.content)
 
--- scraper_arxiv.py
+++ scraper_arxiv.py
@@ -3,12 +3,27 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests 
 
+subject = ["Physics", "Mathematics", "Computer_Science", "Biology", "Engineering", "Statistics", "Economics", "Finance"]
+
+subfield_physics = ["astro-ph", "cond-mat", "gr-qc", "hep-ex", "hep-lat", "hep-ph", "hep-th", "math-ph", "nlin", "nucl-ex", "nucl-th", "physics", "quant-ph"]
+
+subfield_maths = ["math.AG", "math.AT", "math.AP", "math.CT", "math.CA", "math.CO", "math.AC", "math.CV", "math.DG", "math.DS", "math.FA", "math.GM", "math.GN", "math.GT", "math.GR", "math.HO", "math.IT", "math.KT", "math.LO", "math.MP", "math.MG", "math.NT", "math.MA", "math.OA", "math.OC", "math.PR", "math.QA", "math.RT", "math.RA", "math.SP", "math.ST", "math.SG"]
+
 title_csv=[]
 url_csv=[]
 authors_csv=[]
 category_csv=[]
 
-def arxiv():
+def field(subject): 
+	if subject == "Physics" :
+		subsubject = subfield_physics
+	elif subject == "Mathematics" :
+		subsubject = subfield_maths
+	else :
+		subsubject = f"{subject}"	
+	return subsubject
+	
+def arxiv(subject):
  	driver = webdriver.Chrome()
  	URL_arxiv = "https://arxiv.org/search/?query=lecture+notes&searchtype=all&source=header"
  	driver.get(URL_arxiv)
@@ -20,18 +35,23 @@ def arxiv():
  	for ppri in papers:
   		title = ppri.find("p", class_="title is-5 mathjax")
   		authors = ppri.find("p", class_="authors")
-	        url = ppri.find("a", href_="")
-	        category = ppri.find("span", class_="tag is-small is-link tooltip is-tooltip-top")
-	        title_csv.append(title.text.strip())
-  		url_csv.append(url.text.strip())
-  		authors_csv.append(authors.text.strip())
-  		category_csv.append(category.text.strip())
-  		df = pd.DataFrame({'Title':title_csv,'Authors':authors_csv,'Category':category_csv,'Reference':url_csv}) 
-  		df['Authors'] = df['Authors'].str.replace('Authors:', '')
-  		df['Authors'] = df['Authors'].replace('\\n', '', regex=True)
-  		df['Authors'] = df['Authors'].replace('           ', '', regex=True)
-  		df['Reference'] = df['Reference'].str.replace('arXiv:', '')
-  		df.to_csv('arxiv_data.csv', index=False, encoding='utf-8') 
+  		url = ppri.find("a", href_="")
+  		category = ppri.find("span", class_="tag is-small is-link tooltip is-tooltip-top")
+  		print(category.text.strip())
+  		print(field(subject))
+  		input()
+  		if any(category.text.strip() == field(subject) for item in field(subject)) :
+  		        title_csv.append(title.text.strip())
+  		        url_csv.append(url.text.strip())
+  		        authors_csv.append(authors.text.strip())
+  		        category_csv.append(category.text.strip())
+  		        df = pd.DataFrame({'Title':title_csv,'Authors':authors_csv,'Category':category_csv,'Reference':url_csv}) 
+  		        df['Authors'] = df['Authors'].str.replace('Authors:', '')
+  		        df['Authors'] = df['Authors'].replace('\\n', '', regex=True)
+  		        df['Authors'] = df['Authors'].replace('           ', '', regex=True)
+  		        df['Reference'] = df['Reference'].str.replace('arXiv:', '')
+  		        df.to_csv('arxiv_data.csv', index=False, encoding='utf-8') 
+	 		
  
 
 
